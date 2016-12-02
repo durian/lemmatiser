c5d714039729b5428c311ae5c977749037901514
Peter Berck <durian@users.noreply.github.com>
2016-12-02 15:52:50 +0100

    Updates, WIP.

--- compare_wlt.py
+++ compare_wlt.py
@@ -11,11 +11,12 @@ def DBG(*strs):
     if debug:
         sys.stderr.write("DBG:"+"".join(str(strs))+"\n")
 
-testfilename = None
+testfilename = None # -wlt.txt
 goldfilename = None
+statsmode = False # if -wlt is relly -stats
 
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "f:g:", [])
+    opts, args = getopt.getopt(sys.argv[1:], "f:g:s", [])
 except getopt.GetoptError as err:
     print(str(err))
     sys.exit(1)
@@ -24,30 +25,74 @@ for o, a in opts:
         testfilename = a
     elif o in ("-g"): #lookup a lemma
          goldfilename = a
+    elif o in ("-s"): #lookup a lemma
+         statsmode = True
     else:
         assert False, "unhandled option"
 
 stats = Counter()
-    
+
 with open(testfilename, 'r') as f:
     with open(goldfilename, 'r') as g:
         for fl in f:
-            gl = g.readline()
-            fbits = fl.split()
-            gbits = gl.split()
-            if len(fbits) != 3 or len(gbits) != 3:
+            if len(fl) > 0 and fl[0] == "#":
                 continue
-            # Lemma
-            if fbits[1] == gbits[1]:
-                stats["lemma correct"] += 1
+            gl = g.readline()
+            if len(gl) > 0 and gl[0] == "#":
+                continue
+            fl = fl.strip()
+            gl = gl.strip()
+            fbits = fl.split('\t')
+            gbits = gl.split('\t')
+            if not statsmode:
+                if len(fbits) != 3 or len(gbits) != 3:
+                    continue
+                # Lemma
+                if fbits[1] == gbits[1]:
+                    stats["lemma correct"] += 1
+                else:
+                    stats["lemma wrong"] += 1
+                # Tag
+                if fbits[2] == gbits[2]:
+                    stats["tag correct"] += 1
+                else:
+                    stats["tag wrong"] += 1
+                stats["lines"] += 1
             else:
-                stats["lemma wrong"] += 1
-            # Tag
-            if fbits[2] == gbits[2]:
-                stats["tag correct"] += 1
-            else:
-                stats["tag wrong"] += 1
+                # Ἡροδότου        Ἡρόδοτος        Ne-s---mg-      /Ἡροδότου/Ἡρόδοτος/Ne-s---mg-/1/greek_Haudag/   one lemma, but different pos tag
+                if len(fbits) != 5 or len(gbits) != 3:
+                    print( fl, gl )
+                    continue
+                # Lemma, also pos 1
+                if fbits[1] == gbits[1]:
+                    stats["lemma correct"] += 1
+                else:
+                    stats["lemma wrong"] += 1
+                # tag (t) is /ἄνευ/ἄνευ/R----------n/20/proiel/
+                t = fbits[3]
+                tbits = t.split("/") # ['', 'ἄνευ', 'ἄνευ', 'R----------n', '20', 'proiel', '']
+                if len(tbits) != 7:
+                    continue
+                corpus = tbits[5]
+                #print( "["+fbits[2]+"]", "["+gbits[2]+"]")
+                if fbits[2] == gbits[2]: #fbits[2] or tbits[3]
+                    stats["tag correct"] += 1
+                    stats["tag correct"+"/"+corpus] += 1
+                    stats[fbits[4]+"/correct"] += 1
+                    stats[fbits[4]+"/"+corpus+"/correct"] += 1
+                else:
+                    stats["tag wrong"] += 1
+                    stats["tag wrong"+"/"+corpus] += 1
+                    stats[fbits[4]+"/wrong"] += 1
+                    stats[fbits[4]+"/"+corpus+"/wrong"] += 1
+                # extra info
+                stats[fbits[4]] += 1
+                stats["lines"] += 1
 
+for stat, count in sorted(stats.items()):
+    #for stat, count in lemmatiser_stats.most_common():
+    print( "# {0:<71} {1:5n}".format(stat, count) )
+            
 total_lemma = stats["lemma correct"] + stats["lemma wrong"]
 for x in ["lemma correct", "lemma wrong"]:
     print( "# {0:<40} {1:5n} {2:6.2f}".format(x, stats[x], stats[x]*100.0/total_lemma ) ) 

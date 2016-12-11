283590478adb27d713942b75a23628e63d9c7b60
Peter Berck <durian@users.noreply.github.com>
2016-12-11 15:04:36 +0100

    Problems with punctuation.

--- lemmatiser_cltk2.py
+++ lemmatiser_cltk2.py
@@ -6,8 +6,9 @@ import getopt, sys, os
 from unicodedata import normalize
 
 '''
+THIS ONE READS COLUMN FORMAT
 (venv) durian:lemmatiser_new pberck
-python3 lemmatiser_cltk.py  -f hdt_Books_forFrog.col.s
+python3 lemmatiser_cltk.py  -f hdt_Books_forFrog.col.nutt
 '''
 
 debug = False
@@ -47,27 +48,15 @@ with open(filename, 'r') as f:
     with open(outfilename, 'w') as of:
         for l in f:
             l = l.strip()
-            ln = normalize('NFC', l)
-            lemmas = cltk_lemmatiser.lemmatize( ln )
-            tags   = cltk_tagger.tag_ngram_123_backoff( ln )
-            words = ln.split()
-            '''
-            print( "words" )
-            for w in words:
-                print( w )
-            print( "lemmas" )
-            for l in lemmas:
-                print( l )
-            print( "tags" )
-            for t in tags:
-                print( t )
-            '''
-            assert len(words) == len(lemmas) == len(tags), "Truncated output? %r %r %r %r" % (len(words), len(lemmas), len(tags), lc)
-            for w in words:
-                lemma = lemmas.pop()
-                tag = tags.pop()
-                if len(tag) != 2:
-                    print( "ERROR IN TAG" )
-                    sys.exit(2)
-                print( w, "\t", lemma, "\t", tag[1], file=of )
+            bits = l.split()
+            if len(bits) != 3:
+                continue
+            w = normalize('NFC', bits[0])
+            l = normalize('NFC', bits[1])
+            t = normalize('NFC', bits[2])
+            lemma = cltk_lemmatiser.lemmatize( w )
+            # καὶ [('καὶ', 'C--------')]
+            # δι’ [('δι', None), ('’', '---------')]
+            tag   = cltk_tagger.tag_ngram_123_backoff( w )[0]
+            print( w, "\t", lemma, "\t", tag[1], file=of )
             lc += 1

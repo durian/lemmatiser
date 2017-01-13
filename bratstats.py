2b2bc625f3dbf941dbe0cb997070284968ecb63a
Peter Berck <durian@users.noreply.github.com>
2017-01-13 13:24:11 +0100

    WIP.

--- bratstats.py
+++ bratstats.py
@@ -18,9 +18,11 @@ number of complements
 
 number of complements that contain a root
 average number of word of complements, in general and for direct vs indirect complements
+
 number of direct complements
 number of indirect complements
 number of NP complements
+
 number of mixed complements
 do attitude verbs occur with direct complements?
 do δὴ and δή occur in complements? (mogen samen genomen worden)
@@ -35,6 +37,19 @@ The .txt files for the general statistics, number of sentences/words, etc.
 The .ann files to count the complements etc.
 '''
 
+class Complement:
+    # T13	Complement 96 153	ἐν μέρει τινὶ τῆς χώρας Κύκλωπες καὶ Λαιστρυγόνες οἰκῆσαι
+    def __init__(self, id):
+        self.id   = id
+        self.type = "?"
+        self.words = []
+        self.head = "?" 
+        self.span = [] # pairs of [start, end]
+    def __repr__(self):
+        return "|"+self.id+"|"
+    def __str__(self):
+        return "Complement:"+self.id+" type:"+self.type+" words:"+str(len(self.words))+" span:"+repr(self.span)
+    
 filenames = [] # Should be the *.txt files, and we figure out the .ann names from these
 filename  = None
 
@@ -55,6 +70,9 @@ stats["wc"] = 0 # word count
 stats["sc"] = 0 # sentence count
 #
 stats["compl"] = 0 # number of complements
+stats["compl_i"] = 0 # indirect
+stats["compl_d"] = 0 # direct
+stats["compl_np"] = 0 
 
 long = {}
 long["fc"] = "Aantal bestanden"
@@ -62,7 +80,9 @@ long["wc"] = "Aantal woorden"
 long["sc"] = "Aantal zinnen"
 #
 long["compl"] = "Aantal 'Complement' annotaties"
-
+long["compl_i"] = "Aantal indirekte 'Complement' annotaties"
+long["compl_d"] = "Aantal direkte 'Complement' annotaties"
+long["compl_np"] = "Aantal NP 'Complement' annotaties"
 # ----
 # Process
 # ----
@@ -94,24 +114,52 @@ for filename in filenames:
         print( "ERROR: annotation file not found.", file=sys.stderr )
         sys.exit(1)
     print( "FILE:", filename, file=sys.stderr )
-    complement = {}
+    complements = {} # Complements by id
     with open(filename, 'r') as f:
         for l in f:
             l = l.strip()
             bits = l.split("\t")
-            if len(bits) != 3:
+            if len(bits) < 2:
                 continue
             # T4      Complement 46 156       αὖθις μείζονι...
             ann_id   = bits[0]
             ann_info = bits[1].split()
             ann_type = ann_info[0]
-            words = [ normalize('NFC', w) for w in bits[2].split() ]
-            if ann_type == "Complement":
+            if len(bits) == 3:
+                words = [ normalize('NFC', w) for w in bits[2].split() ]
+            else:
+                words = []
+            if ann_type == "Complement": #compl-head compl-chunk ?
                 # Write out/count the current complement
-                if complement:
-                    print( repr(complement) )
+                # Or save all of them, because the order is random in the .ann files?
+                complements[ann_id] = Complement(ann_id)
+                # spans could look like this:
+                '''
+                T13 Complement 96 153
+                T17 Complement 156 158;163 173
+                '''
+                spans = " ".join(ann_info[1:]) # The string after "Complement"
+                for span in spans.split(";"): # spans are seperated by a ";"
+                    xy = span.split() # and consist of a start and end position
+                    if len(xy) == 2:
+                        complements[ann_id].span.append( [int(xy[0]),int(xy[1])] )
+                    else:
+                        print( "ERROR" )
+                        sys.exit(2)
                 stats["compl"] += 1
-                #complement[
-    
+                complements[ann_id].words = words
+                print( complements[ann_id] )
+            # We build up the Complements first, count when file is done
+            if ann_type == "compl-type":
+                # compl-type T6 indirect
+                # print( bits )
+                compl_id   = ann_info[1] #points to the Complement id
+                compl_type = ann_info[2]
+                complements[compl_id].type = compl_type
+                print( complements[compl_id] )
+
 for stat, count in sorted(stats.items()):
-    print( "{0:<40} {1:5n}".format(long[stat], count) )
+    try:
+        print( "{0:<40} {1:5n}".format(long[stat], count) )
+    except KeyError:
+        print( "# {0:<40} {1:5n}".format(stat, count) )
